667933d4e24f883d00cbc0110a07473e0029d6f5
raivels <raivo.snore@gmail.com>
2023-04-16 21:30:45 +0300

    First Commit

---
+++ u3.py
@@ -0,0 +1,69 @@
+
+def get_input():
+    num1 = float(input("Ievadiet 1. skaitli: "))
+    num2 = float(input("Ievadiet 2. skaitli: "))
+    operator = input("Ievadiet operatoru (+, -, *, /, %): ")
+    return num1, num2, operator
+
+# Aprēķina summu
+
+
+def add(num1, num2):
+    return num1 + num2
+
+# Aprēķina starpību
+
+
+def subtract(num1, num2):
+    return num1 - num2
+
+# Aprēķina reizinājumu
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+# Aprēķina dalījumu
+
+
+def divide(num1, num2):
+    if num2 == 0:
+        return "Nepareizi , dalīt ar nulli nedrikst."
+    else:
+        return num1 / num2
+
+# Aprēķina atlikumu
+
+
+def modulus(num1, num2):
+    if num2 == 0:
+        return "Nepareizi , dalīt ar nulli nedrikst."
+    else:
+        return num1 % num2
+
+# Apvieno iepriekšējās funkcijas, lai veiktu matemātisku darbību atkarībā no ievades
+
+
+def calculator():
+    num1, num2, operator = get_input()
+
+    if operator == "+":
+        print(num1, "+", num2, "=", add(num1, num2))
+    elif operator == "-":
+        print(num1, "-", num2, "=", subtract(num1, num2))
+    elif operator == "*":
+        print(num1, "*", num2, "=", multiply(num1, num2))
+    elif operator == "/":
+        print(num1, "/", num2, "=", divide(num1, num2))
+    elif operator == "%":
+        print(num1, "%", num2, "=", modulus(num1, num2))
+    else:
+        print("Nepareizi, lūdzu, ievadiet pareizo operatoru (+, -, *, /, %).")
+
+
+# Izpilda kalkulatora funkciju, kamēr lietotājs to vēlas
+while True:
+    calculator()
+    choice = input("Vai vēlaties turpināt? (jā/nē): ")
+    if choice.lower() != "jā":
+        break
